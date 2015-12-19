b418354f9bd0ba33ae40bfb098514466018b2c13
liudeng <hfutld@yeah.net>
2015-12-20 01:15:44 +0800

    memory leak?

--- library/src/main/java/com/devbrackets/android/exomedia/EMVideoView.java
+++ library/src/main/java/com/devbrackets/android/exomedia/EMVideoView.java
@@ -283,9 +283,9 @@ public class EMVideoView extends RelativeLayout implements AudioCapabilitiesRece
     private RenderBuilder getRendererBuilder(VideoType renderType, Uri uri, MediaUtil.MediaType defaultMediaType) {
         switch (renderType) {
             case HLS:
-                return new HlsRenderBuilder(getContext(), getUserAgent(), uri.toString());
+                return new HlsRenderBuilder(getContext().getApplicationContext(), getUserAgent(), uri.toString());
             default:
-                return new ExtractorRendererBuilder(getContext(), getUserAgent(), uri.toString());
+                return new ExtractorRendererBuilder(getContext().getApplicationContext(), getUserAgent(), uri.toString());
         }
     }
 
