9dd1985593500f24ef971fbd89cc1e22893ec406
liudeng <hfutld@yeah.net>
2015-12-20 22:25:12 +0800

    full screnn

--- library/src/main/java/com/devbrackets/android/exomedia/EMFullScreenVideoPlayerActivity.java
+++ library/src/main/java/com/devbrackets/android/exomedia/EMFullScreenVideoPlayerActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.devbrackets.android.exomedia.listener.EMVideoViewControlsCallback;
@@ -18,7 +19,7 @@ import com.devbrackets.android.exomedia.widget.DefaultControls;
  * <p>
  * <b><em>NOTE:</em></b> the EMVideoView setup is done in the {@link}
  */
-public class EMFullScreenVideoPlayerActivity extends Activity implements MediaPlayer.OnPreparedListener {
+public class EMFullScreenVideoPlayerActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
 
     static String VideoUrlToPlay = "";
     static MediaUtil.MediaType VideoMediaType;
@@ -40,92 +41,17 @@ public class EMFullScreenVideoPlayerActivity extends Activity implements MediaPl
 
         emVideoView = (EMVideoView)findViewById(R.id.video_play_activity_video_view);
         emVideoView.setOnPreparedListener(this);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            fullScreenListener = new FullScreenListener();
-        }
-
-        goFullscreen();
         emVideoView.setVideoViewControlsCallback(new DefaultControlsCallback());
         emVideoView.setVideoURI(Uri.parse(VideoUrlToPlay), VideoMediaType);
         emVideoView.setIsFullScreen(true);
+        emVideoView.setReleaseOnDetachFromWindow(false);
     }
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-        //Starts the video playback as soon as it is ready
         emVideoView.start(true);
     }
 
-
-    private FullScreenListener fullScreenListener;
-
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        exitFullscreen();
-    }
-
-    private void goFullscreen() {
-        setUiFlags(true);
-    }
-
-    private void exitFullscreen() {
-        //setUiFlags(true);
-    }
-
-    /**
-     * Applies the correct flags to the windows decor view to enter
-     * or exit fullscreen mode
-     *
-     * @param fullscreen True if entering fullscreen mode
-     */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private void setUiFlags(boolean fullscreen) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            View decorView = getWindow().getDecorView();
-            if (decorView != null) {
-                decorView.setSystemUiVisibility(fullscreen ? getFullscreenUiFlags() : View.SYSTEM_UI_FLAG_VISIBLE);
-                decorView.setOnSystemUiVisibilityChangeListener(fullScreenListener);
-            }
-        }
-    }
-
-    /**
-     * Determines the appropriate fullscreen flags based on the
-     * systems API version.
-     *
-     * @return The appropriate decor view flags to enter fullscreen mode when supported
-     */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private int getFullscreenUiFlags() {
-        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        }
-
-        return flags;
-    }
-
-    /**
-     * Listens to the system to determine when to show the default controls
-     * for the {@link EMVideoView}
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private class FullScreenListener implements View.OnSystemUiVisibilityChangeListener {
-        @Override
-        public void onSystemUiVisibilityChange(int visibility) {
-            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                emVideoView.showDefaultControls();
-            }
-        }
-    }
 
     /**
      * A Listener for the {@link DefaultControls}
@@ -134,7 +60,7 @@ public class EMFullScreenVideoPlayerActivity extends Activity implements MediaPl
     private class DefaultControlsCallback implements EMVideoViewControlsCallback {
         @Override
         public boolean onFullScreenClicked() {
-            EMFullScreenVideoPlayerActivity.this.finish();
+            onBackPressed();
             return true;
         }
 
@@ -160,7 +86,6 @@ public class EMFullScreenVideoPlayerActivity extends Activity implements MediaPl
 
         @Override
         public boolean onControlsHidden() {
-            goFullscreen();
             return false;
         }
     }
